5357954ed8fbb1807cb872859f32aefce4e37178
david-doit-intl <david@doit-intl.com>
2021-03-31 09:47:47 +0100

    Feat: Fix formatting and split out class
    Signed-off-by: david-doit-intl <david@doit-intl.com>

--- src/main/java/com/doit/dataflowtemplates/BigQueryToPubSub.java
+++ src/main/java/com/doit/dataflowtemplates/BigQueryToPubSub.java
@@ -3,14 +3,9 @@ package com.doit.dataflowtemplates;
 import com.google.api.client.googleapis.util.Utils;
 import com.google.api.services.bigquery.model.TableRow;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.PipelineResult.State;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
-import org.apache.beam.sdk.options.Description;
-import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.options.Validation.Required;
-import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.TypeDescriptor;
 
@@ -33,27 +28,6 @@ import org.apache.beam.sdk.values.TypeDescriptor;
  * </pre>
  */
 public class BigQueryToPubSub {
-
-  /** The custom options supported by the pipeline. Inherits standard configuration options. */
-  public interface Options extends PipelineOptions {
-
-    @Description(
-        "The name of the topic which data should be published to. "
-            + "The name should be in the format of projects/<project-id>/topics/<topic-name>.")
-    @Required
-    ValueProvider<String> getOutputTopic();
-
-    void setOutputTopic(ValueProvider<String> value);
-
-    @Description(
-        "The name of the Big Query table in full "
-            + "The name should be in the format of project:dataset.table")
-    @Required
-    ValueProvider<String> getBigQueryTableName();
-
-    void setBigQueryTableName(ValueProvider<String> value);
-  }
-
   /**
    * Main entry-point for the pipeline. Reads in the command-line arguments, parses them, and
    * executes the pipeline.
@@ -61,22 +35,17 @@ public class BigQueryToPubSub {
    * @param args Arguments passed in from the command-line.
    */
   public static void main(String[] args) {
-    // Parse the user options passed from the command-line
-    final Options options =
-        PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
-
-    run(options);
+    final var options =
+        PipelineOptionsFactory.fromArgs(args).withValidation().as(CustomOptions.class);
+    final var pipeline = Pipeline.create(options);
+    run(pipeline, options);
   }
-
   /**
    * Executes the pipeline with the provided execution parameters.
    *
-   * @param options The execution parameters.
+   * @param pipeline The dataflow pipeline.
    */
-  public static void run(final Options options) {
-    // Create the pipeline.
-    final Pipeline pipeline = Pipeline.create(options);
-
+  public static void run(final Pipeline pipeline, final CustomOptions options) {
     /*
      * Steps:
      *  1) Read from Big Query source
@@ -87,16 +56,16 @@ public class BigQueryToPubSub {
         .apply(
             "Read from BigQuery query",
             BigQueryIO.readTableRows()
-              .from(options.getBigQueryTableName())
-              .withTemplateCompatibility()
-              .withoutValidation())
+                .from(options.getBigQueryTableName())
+                .withTemplateCompatibility()
+                .withoutValidation())
         .apply(
             "TableRows -> PubSub Messages",
             MapElements.into(TypeDescriptor.of(String.class))
                 .via(
                     (final TableRow item) -> {
                       // Data should be immutable from one PTransform to the next
-                      final TableRow tableRow = item.clone();
+                      final var tableRow = item == null ? new TableRow() : item.clone();
                       tableRow.setFactory(Utils.getDefaultJsonFactory());
                       return tableRow.toString();
                     }))
